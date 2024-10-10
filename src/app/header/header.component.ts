967760945e80d95c2c9c473b29df059abf15e2db
Asif Mahmud <asifmahmud3472@gmail.com>
2024-10-10 19:45:58 -0400

    separated main menu and top menu from header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,11 @@
 import { Component } from "@angular/core";
+import { TopMenuComponent } from "./top-menu/top-menu.component";
+import { MainMenuComponent } from "./main-menu/main-menu.component";
 
 @Component({
     standalone: true,
     selector: 'app-header',
+    imports: [TopMenuComponent, MainMenuComponent],
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css']
 })
