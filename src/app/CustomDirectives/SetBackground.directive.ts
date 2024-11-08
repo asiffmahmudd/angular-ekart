a30f2596ee5413535a0c29287ecfd3817bbb8a28
Asif Mahmud <asifmahmud3472@gmail.com>
2024-11-07 20:37:46 -0500

    added custom directives

--- src/app/CustomDirectives/SetBackground.directive.ts
+++ src/app/CustomDirectives/SetBackground.directive.ts
@@ -1,5 +1,5 @@
 import { style } from "@angular/animations";
-import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, Input, OnInit, Renderer2 } from "@angular/core";
 
 
 @Directive({
@@ -7,12 +7,18 @@ import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
     selector: '[setBackground]'
 })
 export class SetBackground implements OnInit{
+
+    // @Input('setBackground') backColor: String = '#36454F';
+    // @Input() textColor: String = 'White';
+
+    @Input('setBackground') changeBackAndTextColor: { backColor: String; textColor: String; }
+
     constructor(private element: ElementRef, private renderer: Renderer2){}
 
     ngOnInit() {
         // this.element.nativeElement.style.backgroundColor = '#36454F';
         // this.element.nativeElement.style.color = 'white';
-        this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', '#36454F');
-        this.renderer.setStyle(this.element.nativeElement, 'color', 'white');
+        this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', this.changeBackAndTextColor.backColor);
+        this.renderer.setStyle(this.element.nativeElement, 'color', this.changeBackAndTextColor.textColor);
     }
 }
